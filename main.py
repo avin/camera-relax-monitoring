a45f3f709a4eeddd19e23e17b691083b02f21da8
a.vinogradov <a.vinogradov@bpcbt.com>
2024-04-21 12:24:36 +0300

    upd

--- main.py
+++ main.py
@@ -5,29 +5,42 @@ import time
 import tkinter as tk
 from queue import Queue
 
-SLEEP_TIME = 10 # 10sec
-SCREEN_TIME = 1800/SLEEP_TIME # 30min
-ADD_MORE_TIME = 300/SLEEP_TIME # 5min
-RELAX_TIME = 180/SLEEP_TIME # 3min
+# SLEEP_TIME = 10  # 10sec
+# SCREEN_TIME = 1800 / SLEEP_TIME  # 30min
+# ADD_MORE_TIME = 300 / SLEEP_TIME  # 5min
+# RELAX_TIME = 180 / SLEEP_TIME  # 3min
+# ENABLE_GPU = True
+
+ENABLE_GPU = not not cv2.cuda.getCudaEnabledDeviceCount()
+SLEEP_TIME = .5  # 10sec
+SCREEN_TIME = 30  # 30min
+ADD_MORE_TIME = 10  # 5min
+RELAX_TIME = 10  # 3min
+
+print(f'ENABLE_GPU={ENABLE_GPU}')
+
+# -----------------------------------------------------------
+
+notification_shown = False
+in_camera_count = 0
+off_camera_count = 0
 
 
 def video_analysis(message_queue):
     global in_camera_count, off_camera_count, notification_shown
     camera = cv2.VideoCapture(0)
-    notification_shown = False
-    in_camera_count = 0
-    off_camera_count = 0
 
     while True:
         ret, frame = camera.read()
         if not ret:
             continue
 
-        bbox, label, conf = cv.detect_common_objects(frame, model='yolov4', enable_gpu=False)
+        bbox, label, conf = cv.detect_common_objects(frame, model='yolov4-tiny', enable_gpu=ENABLE_GPU)
         if 'person' in label:
             print("Человек обнаружен в кадре.", in_camera_count, off_camera_count)
             if not notification_shown:
                 in_camera_count += 1
+                off_camera_count = max(off_camera_count - 1, 0)
             if in_camera_count >= SCREEN_TIME and not notification_shown:
                 in_camera_count = 0
                 off_camera_count = 0
@@ -36,6 +49,7 @@ def video_analysis(message_queue):
         else:
             print("Человека нет в кадре.", in_camera_count, off_camera_count)
             off_camera_count += 1
+            in_camera_count = max(in_camera_count - 1, 0)
             if notification_shown and off_camera_count >= RELAX_TIME:
                 off_camera_count = 0
                 message_queue.put("hide")
@@ -46,27 +60,22 @@ def video_analysis(message_queue):
 
         time.sleep(SLEEP_TIME)
 
+
 def add_more_time(root):
     global in_camera_count, off_camera_count, notification_shown
     in_camera_count = SCREEN_TIME - ADD_MORE_TIME
     notification_shown = False
     root.withdraw()
 
+
 def manage_window(root, message_queue):
-    label = tk.Label(root, text="Сделай перерыв", font=("Arial", 24))
-    label.pack(expand=True)
-
-    button = tk.Button(root, text="Еще 5 минуток", font=("Arial", 14), command=lambda: add_more_time(root))
-    button.pack(pady=20)
-
-    root.geometry("600x600")
-
     while True:
         message = message_queue.get()
         if message == "show":
-            root.deiconify()  # Показать окно
+            root.deiconify()
         elif message == "hide":
-            root.withdraw()  # Скрыть окно
+            root.withdraw()
+
 
 def center_window(root, width=600, height=600):
     # Получаем размеры экрана
@@ -80,16 +89,32 @@ def center_window(root, width=600, height=600):
     # Устанавливаем размеры и позицию окна
     root.geometry(f'{width}x{height}+{x}+{y}')
 
-if __name__ == "__main__":
-    message_queue = Queue()
 
-    # Создание основного окна GUI
+def create_window():
     root = tk.Tk()
     root.protocol("WM_DELETE_WINDOW", lambda: None)
     root.attributes("-topmost", True)
     root.attributes("-toolwindow", True)
+    root.title("Напоминание о перерыве")
+
+    label = tk.Label(root, text="Сделай перерыв", font=("Arial", 24))
+    label.pack(expand=True)
+
+    button = tk.Button(root, text="Еще 5 минуток", font=("Arial", 14), command=lambda: add_more_time(root))
+    button.pack(pady=20)
+
+    root.geometry("600x600")
+
     center_window(root)
     root.withdraw()  # Сначала окно будет скрыто
+
+    return root
+
+
+if __name__ == "__main__":
+    message_queue = Queue()
+
+    root = create_window()
 
     # Запуск потока управления окном
     window_thread = threading.Thread(target=manage_window, args=(root, message_queue))
